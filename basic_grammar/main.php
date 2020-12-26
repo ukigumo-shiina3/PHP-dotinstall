1b7a276159ed8bb442fe6f52f47afcf083089593
Daisuke Tanaka <daisukeyao@outlook.jp>
2020-12-26 12:13:40 +0900

    [Add]7-13

--- basic_grammar/main.php
+++ basic_grammar/main.php
@@ -1,12 +1,8 @@
+<!-- ヒアドキュメント(改行や字下げの保持) -->
 <?php
 
 $name = 'taguchi';
 
-echo 'Hello ' . $name . PHP_EOL;   
-echo 'Hi ' . $name . PHP_EOL;
-
-// $text = <<<'EOT' // nowdoc
-// $text = <<<"EOT" // heredoc
 $text = <<<EOT
 hello! $name
     this is looooong
@@ -14,4 +10,48 @@ text!
 
 EOT;
 
-echo $text;
+echo $text; 
+
+// 変数を使った演算
+<?php
+
+$price = 500;
+
+$price += 100; #600
+$price *= 100; #60000
+
+$price++; #60001
+$price--; #60000
+
+echo $price . PHP_EOL;
+
+// 定数(define,const)
+<?php
+define('NAME', 'taguchi')
+echo NAME . PHP_EOL;
+
+const NAME = 'dotinstall';
+echo NAME . PHP_EOL;
+
+// キャスト(データ型の変更)
+<?php
+
+$x = (float)5; //string→floatに変更
+$y = (string)3.2; //string→floatに変更
+
+var_dump($x);
+var_dump($y);
+
+// if文
+<?php
+// $score = 85;
+// $score = 70;
+$score = 40;
+
+if ($score >= 80) {
+    echo 'Great!' . PHP_EOL;
+} elseif ($score >= 60) {
+    echo 'Good!' . PHP_EOL;
+} else {
+    echo 'OK!' . PHP_EOL;
+}
